45852a7bf36e7d5700081edbe192c3a9b0439a38
wangxin <408995858@qq.com>
2021-02-15 16:04:35 +0800

    footer

---
+++ src/components/footer/index.vue
@@ -0,0 +1,41 @@
+<!-- Created by wangxin on 2020/11/20 -->
+<template>
+  <div class="footer">
+    <p>{{ config.copy_right }}</p>
+    <p>{{ config.icp }}</p>
+    <p>{{ config.tip }}</p>
+    <p>
+      <a
+        :href="config.beian_url"
+        target="_blank"
+      >
+        <img
+          src="~@/assets/img/gwab.png"
+        >
+        <span>{{ config.beian }}</span>
+      </a>
+    </p>
+  </div>
+</template>
+<style scoped lang="scss">
+.footer{
+  text-align:center;
+  padding-bottom:40px;
+  p{
+    color:#ffffff;
+    font-size:24px;
+    line-height:36px;
+    a{
+      color:#ffffff;
+      display:flex;
+      align-items:center;
+      justify-content:center;
+    }
+    img{
+      width:27px;
+      height:30px;
+      margin-right:10px;
+    }
+  }
+}
+</style>
---
+++ src/components/header/index.vue
@@ -0,0 +1,49 @@
+<!-- Created by wangxin on 2020/11/20 -->
+<template>
+  <div class="header">
+    <!--    <img src="~@/assets/img/logo.png" alt="" />-->
+    <a
+      @click="onDownload(config.download_url)"
+      class="btn"
+      target="_blank"
+    >
+      立即下载
+    </a>
+  </div>
+</template>
+<script>
+export default {
+  methods:{
+    onDownload(url){
+      var elemIF = document.createElement('iframe')
+      elemIF.src = url
+      elemIF.style.display = 'none'
+      document.body.appendChild(elemIF)
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+.header{
+  padding:40px 35px;
+  background:#ffffff;
+  display:flex;
+  justify-content:space-between;
+  align-items:center;
+  img{
+    width:358px;
+    height:100px;
+  }
+  .btn{
+    width:220px;
+    height:80px;
+    background:#e73535;
+    color:#ffffff;
+    line-height:80px;
+    text-align:center;
+    border-radius:40px;
+    font-size:36px;
+  }
+}
+</style>
+
